4498ec98f0476514060a520d04086b1984dd7ba7
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2021-01-03 05:39:00 +0100

    Add simultaneous search across US, JP & FR iTunes Store

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,161 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Head from "next/head";
+import useSWR from "swr";
+import {
+  Box,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  VStack,
+  HStack,
+  Tag,
+  Text,
+  Image,
+  Divider,
+  Badge,
+  StackDivider,
+  Link,
+  CircularProgress,
+} from "@chakra-ui/react";
+import { SearchIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+
+import countries from "../data/countries.js";
+
+function Results({ country, albumID }) {
+  const { data, error } = useSWR(
+    `https://itunes.apple.com/lookup?country=${country.code}&lang=en&id=${albumID}&entity=song&limit=200`
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <CircularProgress isIndeterminate color="teal.500" />;
+  if (data.errorMessage) return <div>{data.errorMessage}</div>;
+
+  const albums = data.results.filter(
+    (result) => result.collectionType === "Album"
+  );
+
+  return (
+    <VStack spacing={4}>
+      <HStack>
+        <Text>{country.emoji}</Text>
+        <Text>{country.name}</Text>
+      </HStack>
+      <Divider />
+      {data.resultCount === 0 ? (
+        <div>Not Found</div>
+      ) : (
+        <>
+          <Text>
+            {data.resultCount} results ({albums.length} albums)
+          </Text>
+          <VStack spacing={4} divider={<StackDivider borderColor="gray.200" />}>
+            {albums.map((album) => {
+              const songs = data.results.filter(
+                (result) =>
+                  result.collectionId === album.collectionId &&
+                  result.kind === "song"
+              );
+              let isStreamable = false;
+              if (songs.length === 0) {
+                isStreamable = true;
+              } else {
+                isStreamable = songs.find((song) => song.isStreamable === true);
+              }
+              const releaseDate = new Date(album.releaseDate);
+              return (
+                <VStack key={album.collectionId}>
+                  <Image
+                    boxSize="100px"
+                    objectFit="cover"
+                    src={album.artworkUrl100}
+                    alt={album.collectionName}
+                  />
+                  <Link
+                    href={album.collectionViewUrl}
+                    color="teal.500"
+                    isExternal
+                  >
+                    <Text>
+                      {album.collectionName} by {album.artistName} (
+                      {releaseDate.getFullYear()})
+                    </Text>
+                  </Link>
+                  <Text>{album.primaryGenreName}</Text>
+                  <Text>Released on {releaseDate.toDateString()}</Text>
+                  <Text>{album.trackCount} tracks</Text>
+                  <Badge colorScheme={isStreamable ? "green" : "red"}>
+                    Stream on Apple Music
+                  </Badge>
+                  <Badge colorScheme={album.collectionPrice ? "green" : "red"}>
+                    Buy on iTunes Store
+                  </Badge>
+                </VStack>
+              );
+            })}
+          </VStack>
+        </>
+      )}
+    </VStack>
+  );
+}
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setSearchQuery(event.target.value);
+    }
+  };
+  const selectedCountries = Object.entries(countries)
+    .map(([_, value]) => value)
+    .filter(
+      (country) =>
+        country.code === "US" || country.code === "JP" || country.code === "FR"
+    );
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>iTunes Store Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Box w="100%" p={4}>
+        <VStack>
+          <HStack>
+            {selectedCountries.map((country) => (
+              <Tag key={country.code}>
+                {country.emoji} {country.name}
+              </Tag>
+            ))}
+          </HStack>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input
+              type="text"
+              placeholder="Search..."
+              onKeyDown={handleKeyDown}
+            />
+          </InputGroup>
+          {searchQuery && (
+            <HStack
+              spacing={32}
+              divider={<StackDivider borderColor="gray.200" />}
+            >
+              {selectedCountries.map((country) => (
+                <Results
+                  key={country.code}
+                  country={country}
+                  albumID={searchQuery}
+                />
+              ))}
+            </HStack>
+          )}
+        </VStack>
+      </Box>
     </div>
-  )
+  );
 }
